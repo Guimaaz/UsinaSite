8a595cfa257307bfeb8364d050a1762e62c3c563
Guimaaz <gustavoguimaraesr@gmail.com>
2024-10-11 18:11:37 -0300

    button form and js to prevent default

---
+++ js/FaleConosco.js
@@ -0,0 +1,30 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+     
+    event.preventDefault();
+    
+   
+    const assunto = document.getElementById('assunto').value.trim();
+    const nome = document.getElementById('nome').value.trim();
+    const sobrenome = document.getElementById('sobrenome').value.trim();
+    const numero = document.getElementById('numero').value.trim();
+    const email = document.getElementById('email').value.trim();
+
+    
+    if (!assunto || !nome || !sobrenome || !numero || !email) {
+        alert('Por favor, preencha todos os campos antes de enviar.');
+        return; 
+    }
+
+   
+    console.log("Assunto:", assunto);
+    console.log("Nome:", nome);
+    console.log("Sobrenome:", sobrenome);
+    console.log("Número:", numero);
+    console.log("Email:", email);
+
+    
+    alert('Contato enviado com sucesso!');
+
+    
+    this.reset();
+});
