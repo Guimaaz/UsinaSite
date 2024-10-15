39d74bdba37a151b3c8bd376f63bd9d360f8a9d6
Guimaaz <gustavoguimaraesr@gmail.com>
2024-10-15 17:06:55 -0300

    change in css and new history folder

--- js/FaleConosco.js
+++ js/FaleConosco.js
@@ -8,9 +8,10 @@ document.getElementById('contact-form').addEventListener('submit', function(even
     const sobrenome = document.getElementById('sobrenome').value.trim();
     const numero = document.getElementById('numero').value.trim();
     const email = document.getElementById('email').value.trim();
+    const conheceu = document.getElementById('conheceu').value.trim()
 
     
-    if (!assunto || !nome || !sobrenome || !numero || !email) {
+    if (!assunto || !nome || !sobrenome || !numero || !email || !conheceu) {
         alert('Por favor, preencha todos os campos antes de enviar.');
         return; 
     }
@@ -21,6 +22,7 @@ document.getElementById('contact-form').addEventListener('submit', function(even
     console.log("Sobrenome:", sobrenome);
     console.log("Número:", numero);
     console.log("Email:", email);
+    console.log("Conheceu:", conheceu)
 
     
     alert('Mensagem enviado com sucesso!');
