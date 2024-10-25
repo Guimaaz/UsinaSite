75fb4e3dad41365c7f1996d78e2a665f5cd04799
Guilherme Brasil <dev.guilhermebrasil@gmail.com>
2024-10-25 10:32:37 -0300

    fix: fix dynamic navbar

--- js/navbar.js
+++ js/navbar.js
@@ -9,16 +9,13 @@ function updateNavbarForLoggedInUser() {
   const dashboardText = document.getElementById("dashboardText");
 
   if (isUserLoggedIn()) {
-      // Altera o link e o texto do "Login" para "Dashboard"
       loginLink.textContent = "Dashboard";
       loginLink.href = "./dashboard.html";
 
-      // Esconde a imagem e exibe o texto "Dashboard"
       loginIconImage.style.display = "none";
       dashboardText.style.display = "inline";
       loginIconLink.href = "./dashboard.html";
   }
 }
 
-// Atualiza a navbar ao carregar a página
 document.addEventListener("DOMContentLoaded", updateNavbarForLoggedInUser);
