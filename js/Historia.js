0060ace89831d0cbac57284a6b7b5b9c30fdf7c5
Guimaaz <gustavoguimaraesr@gmail.com>
2024-10-25 08:19:59 -0300

    2019 history

--- js/Historia.js
+++ js/Historia.js
@@ -81,4 +81,20 @@ document.getElementById("ano2004").addEventListener("click", function () {
         }, 500);
     }
 });
+document.getElementById("ano2019").addEventListener("click", function () {
+    this.classList.toggle("expanded");
+    const imagem = document.getElementById("imagem2019");
+
+    if (this.classList.contains("expanded")) {
+        imagem.style.display = "block";
+        setTimeout(() => {
+            imagem.style.opacity = "1";
+        }, 10);
+    } else {
+        imagem.style.opacity = "0";
+        setTimeout(() => {
+            imagem.style.display = "none";
+        }, 500);
+    }
+});
 
