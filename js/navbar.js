a2f819b6d6ed74db4087e6a7f46d8d807405f667
Guilherme Brasil <dev.guilhermebrasil@gmail.com>
2024-12-05 12:58:55 -0300

    end

--- js/navbar.js
+++ js/navbar.js
@@ -1,31 +1,31 @@
 async function isUserLoggedIn() {
   try {
-    const response = await fetch('http://127.0.0.1:3333/auth/verify', {
-      method: 'GET',
-      credentials: 'include'
-    })
+    const response = await fetch("http://127.0.0.1:3333/auth/verify", {
+      method: "GET",
+      credentials: "include",
+    });
 
-    const data = await response.json()
-    return data.loggedIn
+    const data = await response.json();
+    return data.loggedIn;
   } catch (e) {
-    console.log(e)
-    return false
+    console.log(e);
+    return false;
   }
 }
 
 async function me() {
   try {
-    const response = await fetch('http://127.0.0.1:3333/auth/me', {
-      method: 'GET',
-      credentials: 'include'
-    })
+    const response = await fetch("http://127.0.0.1:3333/auth/me", {
+      method: "GET",
+      credentials: "include",
+    });
 
-    const data = await response.json()
-    console.log(data.user.type)
-    return data.user.type
+    const data = await response.json();
+    console.log(data.user.type);
+    return data.user.type;
   } catch (e) {
-    console.log(e)
-    return false
+    console.log(e);
+    return false;
   }
 }
 
@@ -36,29 +36,39 @@ async function updateNavbarForLoggedInUser() {
   const cartText = document.getElementById("cartText");
 
   if (await isUserLoggedIn()) {
-    if (await me() === 'ADMIN') {
-      loginIconLink.href = "./pages/DashboardIndex.html";
+    if ((await me()) === "ADMIN") {
+      if (window.location.href === "http://127.0.0.1:5500/index.html") {
+        loginIconImage.style.display = "none";
+        loginIconLink.href = "./pages/DashboardIndex.html";
+      } else {
+        loginIconLink.href = "./DashboardIndex.html";
+        loginIconImage.style.display = "none";
+      }
       loginIconImage.style.display = "none";
       dashboardText.style.display = "inline";
       cartText.style.display = "none";
     } else {
-      loginIconLink.href = "./pages/Loja.html";
+      console.log(window);
+
+      if (window.location.href === "http://127.0.0.1:5500/index.html") {
+        loginIconLink.href = "./pages/Loja.html";
+      } else if (window.location.href !== "http://127.0.0.1:5501/index.html") {
+        loginIconLink.href = "./Loja.html";
+      }
       loginIconImage.style.display = "none";
       cartText.style.display = "inline";
       dashboardText.style.display = "none";
     }
   } else {
-    loginIconLink.href = "./pages/Login.html";
+    if (window.location.href === "http://127.0.0.1:5500/index.html") {
+      loginIconLink.href = "./pages/Login.html";
+    } else {
+      loginIconLink.href = "./Login.html";
+    }
     loginIconImage.style.display = "inline";
     dashboardText.style.display = "none";
     cartText.style.display = "none";
   }
 }
 
-
-async function teste() {
-  console.log(await me() === 'ADMIN')
-}
-
 document.addEventListener("DOMContentLoaded", updateNavbarForLoggedInUser);
-document.addEventListener("DOMContentLoaded", teste);
